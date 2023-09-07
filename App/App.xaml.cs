76f703d09326aa7796c82112a11e7cfb4d5b68e9
JENS-PC\jens <jaehrig.jens@gmail.com>
2023-09-08 01:01:25 +0200

    Add project files.

---
+++ App/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.UI.Xaml;
+using System.Diagnostics;
+using System.Threading;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+
+    private Window m_window;
+}
+
+public static class Program
+{
+    [System.Runtime.InteropServices.DllImport("Microsoft.ui.xaml.dll")]
+    private static extern void XamlCheckProcessRequirements();
+
+    [System.STAThreadAttribute]
+    static void Main(string[] args)
+    {
+        XamlCheckProcessRequirements();
+
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Microsoft.UI.Xaml.Application.Start((p) =>
+        {
+            Microsoft.UI.Dispatching.DispatcherQueue dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+            var context = new Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(dispatcherQueue);
+            System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+            new App();
+        });
+    }
+}
